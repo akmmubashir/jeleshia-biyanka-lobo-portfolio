935ba6bc608e3fcf18dfaf8d9ed13a8167092c06
akmmubashir <akmmubashir@gmail.com>
2024-08-05 11:42:11 +0400

    Update aboutSection.jsx

--- app/components/aboutSection.jsx
+++ app/components/aboutSection.jsx
@@ -96,7 +96,7 @@ export default async function AboutSection() {
           />
         </div>
         <div className="flex">
-          <button className="ms-auto mt-[30px] max-md:mt-[20px] border-2 border-white bg-none hover:bg-white hover:text-[#379683] p-[12px_18px] max-md:p-[8px_14px] font-medium text-[18px] max-2xl:text-[16px] max-md:text-[14px]">
+          <button className="ms-auto mt-[30px] max-md:mt-[20px] border-2 border-white text-white bg-none hover:bg-white hover:text-[#379683] p-[12px_18px] max-md:p-[8px_14px] font-medium text-[18px] max-2xl:text-[16px] max-md:text-[14px]">
             Download Resume
           </button>
         </div>
