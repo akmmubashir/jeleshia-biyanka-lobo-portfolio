a63e7a097e8ae407ed05e89a9ffea2445323577c
akmmubashir <akmmubashir@gmail.com>
2024-08-31 00:59:00 +0530

    fixes

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -7,24 +7,21 @@ import EducationSection from "../components/educationSection";
 import SkillsSection from "../components/skillsSection";
 import ContactSection from "../components/contact";
 import Footer from "../components/footer";
-import { fetchEducationData, fetchProfileData, fetchSkillsData, fetchContactData } from "../utils/utilsService";
 
-export default async function HomePage() {
-  const educationData = await fetchEducationData();
-  const profileData = await fetchProfileData();
-  const skillsData = await fetchSkillsData();
-  const contactData = await fetchContactData();
-
+const HomePage = () => {
   return (
     <div className="bg-[#2a2a2a] overflow-hidden">
       <Header />
       <HeroSection />
       <AboutSection />
       <ExperienceSection />
-      <EducationSection data={educationData} />
-      <SkillsSection data={skillsData} />
-      <ContactSection data={contactData} />
+      <EducationSection />
+      <SkillsSection />
+      <ContactSection />
       <Footer />
     </div>
   );
-}
+};
+
+export default HomePage;
+export const revalidate = 0;
--- app/utils/utilsService.js
+++ app/utils/utilsService.js
@@ -1,91 +1,87 @@
-import axios from 'axios';
+import axios from "axios";
 
 export async function fetchEducationData() {
-	const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
-	const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
+  const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
+  const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
 
-	try {
-		const res = await axios.get(`${strapiUrl}/educations`, {
-			headers: {
-				Authorization: `Bearer ${strapiToken}`,
-			},
-			
-		});
+  try {
+    const res = await axios.get(`${strapiUrl}/educations`, {
+      headers: {
+        Authorization: `Bearer ${strapiToken}`,
+      },
+    });
 
-		if (!res.status === 200) {
-			throw new Error('Failed to fetch data');
-		}
+    if (!res.status === 200) {
+      throw new Error("Failed to fetch data");
+    }
 
-		return res.data.data;
-	} catch (error) {
-		console.error('Error fetching data from Strapi:', error);
-		throw error;
-	}
+    return res.data.data;
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    throw error;
+  }
 }
 
 export async function fetchProfileData() {
-	const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
-	const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
+  const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
+  const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
 
-	try {
-		const res = await axios.get(`${strapiUrl}/profile`, {
-			headers: {
-				Authorization: `Bearer ${strapiToken}`,
-			},
-			
-		});
+  try {
+    const res = await axios.get(`${strapiUrl}/profile`, {
+      headers: {
+        Authorization: `Bearer ${strapiToken}`,
+      },
+    });
 
-		if (!res.status === 200) {
-			throw new Error('Failed to fetch data');
-		}
+    if (!res.status === 200) {
+      throw new Error("Failed to fetch data");
+    }
 
-		return res.data.data;
-	} catch (error) {
-		console.error('Error fetching data from Strapi:', error);
-		throw error;
-	}
+    return res.data.data;
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    throw error;
+  }
 }
 export async function fetchContactData() {
-	const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
-	const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
+  const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
+  const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
 
-	try {
-		const res = await axios.get(`${strapiUrl}/contact-info`, {
-			headers: {
-				Authorization: `Bearer ${strapiToken}`,
-			},
-			
-		});
+  try {
+    const res = await axios.get(`${strapiUrl}/contact-info`, {
+      headers: {
+        Authorization: `Bearer ${strapiToken}`,
+      },
+    });
 
-		if (!res.status === 200) {
-			throw new Error('Failed to fetch data');
-		}
+    if (!res.status === 200) {
+      throw new Error("Failed to fetch data");
+    }
 
-		return res.data.data;
-	} catch (error) {
-		console.error('Error fetching data from Strapi:', error);
-		throw error;
-	}
+    return res.data.data;
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    throw error;
+  }
 }
 export async function fetchSkillsData() {
-	const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
-	const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
+  const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_URL;
+  const strapiToken = process.env.NEXT_PUBLIC_STRAPI_TOKEN;
 
-	try {
-		const res = await axios.get(`${strapiUrl}/skills-lists`, {
-			headers: {
-				Authorization: `Bearer ${strapiToken}`,
-			},
-			
-		});
+  try {
+    const res = await axios.get(`${strapiUrl}/skills-lists`, {
+      headers: {
+        Authorization: `Bearer ${strapiToken}`,
+      },
+    });
 
-		if (!res.status === 200) {
-			throw new Error('Failed to fetch data');
-		}
+    if (!res.status === 200) {
+      throw new Error("Failed to fetch data");
+    }
 
-		return res.data.data;
-	} catch (error) {
-		console.error('Error fetching data from Strapi:', error);
-		throw error;
-	}
+    return res.data.data;
+  } catch (error) {
+    console.error("Error fetching data from Strapi:", error);
+    throw error;
+  }
 }
